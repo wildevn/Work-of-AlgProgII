8ac2450965dc0e5d3de6c0473c90fa39ddaa7317
LinaxT25 <LinaxT25@users.noreply.github.com>
2018-11-24 18:42:45 -0300

    Create Interface.h
    Interface that will be executed in program.

---
+++ Interface.h
@@ -0,0 +1,21 @@
+//Date:21/11/2018
+//Contain the interface of student menagement system 
+#include "Bibliotecas.h"
+
+char interface(void)
+{
+    char option;
+
+    printf("Bem-vindo ao sistema de gerenciamento de aluno ALPHA!\n");
+    printf("\n\n\n");
+    printf("Qual das seguintes opcoes voce deseja realizar ?\n");
+    printf("\nCadastrar novo aluno.(Pressione C)");
+    printf("\nBuscar Aluno.(Pressione B)");
+    printf("\nLer arquivo com listagem de alunos.(Pressione L)");
+    printf("\nGerar arquivo com dados dos alunos aprovados.(Pressione A)");
+    printf("\nGerar arquivo com dados dos alunos reprovados(Pressione R)");
+    printf("\nFechar programa.(Pressione F)\n");
+    option=getchar();
+    
+    return option;
+}
