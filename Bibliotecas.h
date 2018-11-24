adeaebcbe58dcb58f806c590750b0f64489b20a6
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2018-11-24 18:53:51 -0300

    Update Bibliotecas.h

--- Bibliotecas.h
+++ Bibliotecas.h
@@ -1,6 +1,6 @@
 //Date:24/11/2018
 //Include all library used in work
-#include<stdio.h> 
-#include<stdlib.h>
-#include<string.h>
-
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
