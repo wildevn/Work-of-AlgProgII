5fb60d48c4758c515281921ea40e385ad43325fe
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2018-11-26 14:59:01 -0300

    Update no.h

--- no.h
+++ no.h
@@ -1,14 +1,13 @@
 //Data: 26/11/18
-//Nó para a lista de alunos
+//No para a lista de alunos
 
 #include <aluno.h>
 
-struct no_st {
-
+struct no_st
+{
 	aluno* al;
 	struct no_st* ant;
 	struct no_st* prox;
-
 };
 
 typedef struct no_st no;
