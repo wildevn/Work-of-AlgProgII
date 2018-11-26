36317d46672a130b7817d6b4282dd5fd77d8b3c5
LinaxT25 <LinaxT25@users.noreply.github.com>
2018-11-26 19:38:52 -0300

    Update no.h

--- no.h
+++ no.h
@@ -1,13 +1,10 @@
 //Data: 26/11/18
 //No para a lista de alunos
-
 #include <aluno.h>
 
-struct no_st
+typedef struct no_st
 {
 	aluno* al;
 	struct no_st* ant;
 	struct no_st* prox;
-};
-
-typedef struct no_st no;
+} no;
