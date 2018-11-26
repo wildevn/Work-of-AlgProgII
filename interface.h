ff8cf7cde5cc72f97dd3a831cb44eeef916f7d02
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2018-11-26 13:51:37 -0300

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,21 +1,21 @@
 //Date:21/11/2018
-//Contain the interface of student menagement system 
+//Contain the interface of student management system 
 #include "bibliotecas.h"
 
-char interface(void)
+char interface()
 {
     char option;
 
     printf("Bem-vindo ao sistema de gerenciamento de aluno ALPHA!\n");
     printf("\n\n");
-    printf("Qual das seguintes opcoes voce deseja realizar ?\n\n");
+    printf("Qual das seguintes opcoes voce deseja realizar?\n\n");
     printf("Cadastrar novo aluno.(Pressione C)\n");
     printf("Buscar Aluno.(Pressione B)\n");
     printf("Ler arquivo com listagem de alunos.(Pressione L)\n");
     printf("Gerar arquivo com dados dos alunos aprovados.(Pressione A)\n");
-    printf("Gerar arquivo com dados dos alunos reprovados(Pressione R)\n");
+    printf("Gerar arquivo com dados dos alunos reprovados.(Pressione R)\n");
     printf("Fechar programa.(Pressione F)\n");
-    option=getchar();
+    option = getchar();
     
     return option;
 }
